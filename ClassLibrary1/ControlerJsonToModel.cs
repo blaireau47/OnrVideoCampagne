275b0b3d6e4c698d3cff1b56ea325eacfa7ef3cf
Eric Blais <eric.blais@metalogique.com>
2017-10-10 06:31:53 -0400

    Add project files.

---
+++ ClassLibrary1/ControlerJsonToModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace ONRVideo
+{
+    public class ControlerJsonToModel
+    {
+        public void RetrieveModelFromJSon(string _soireeJson)
+        {
+            ONRVideo.ONRCampagneVideoEntities onrEntities = new ONRCampagneVideoEntities();
+
+
+            JObject jsonSoiree = JObject.Parse(_soireeJson);
+            ///Recupere campagne et centrale
+            
+
+            ///sAVE SOIRÉE
+            
+            ONRVideo.Soiree curSoiree = new ONRVideo.Soiree();
+
+           
+
+            curSoiree.campagneId = 1;
+            curSoiree.central = 0;
+            curSoiree.soiree1 = Convert.ToDateTime(jsonSoiree["soiree"].ToString());
+            curSoiree.Id = Convert.ToInt32(jsonSoiree["id"]);
+
+
+
+            onrEntities.Soirees.Add(curSoiree);
+            onrEntities.SaveChanges();
+
+            ///gET sOIRÉE ID
+            int soireeID = curSoiree.Id;
+
+                        
+            IList<JToken> equipeJSONResults = jsonSoiree["teams"].Children().ToList();
+            IList<ONRVideo.Equipe> equipes = new List<ONRVideo.Equipe>();
+
+            foreach (JToken equipe in equipeJSONResults)
+            {
+                ONRVideo.Equipe curEquipe = equipe.ToObject<ONRVideo.Equipe>();
+
+                curEquipe.SoireeId = soireeID;
+                //curSoiree.Equipes.Add(curEquipe);
+                onrEntities.Equipes.Add(curEquipe);
+                equipes.Add(curEquipe);
+
+            }
+
+            onrEntities.SaveChanges();
+
+            
+
+            IList<JToken> benevolesJSONResults = jsonSoiree["volunteers"].Children().ToList();
+            IList<ONRVideo.Volunteer> benevoles = new List<ONRVideo.Volunteer>();
+
+            foreach (JToken benevole in benevolesJSONResults)
+            {
+                ONRVideo.Volunteer curVolunteer = benevole.ToObject<ONRVideo.Volunteer>();
+
+                var teamID = onrEntities.Equipes.Single(x => x.SoireeId == soireeID && x.teamNumber == curVolunteer.teamNumber).Id;
+                curVolunteer.teamID = teamID;
+                onrEntities.Volunteers.Add(curVolunteer);
+                benevoles.Add(curVolunteer);
+
+            }
+            onrEntities.SaveChanges();
+
+
+
+            IList<JToken> transportsJSONResults = jsonSoiree["transports"].Children().ToList();
+            IList<ONRVideo.Transport> transports = new List<ONRVideo.Transport>();
+
+            foreach (JToken transport in transportsJSONResults)
+            {
+                ONRVideo.Transport curTransport = transport.ToObject<ONRVideo.Transport>();
+                curTransport.teamId = onrEntities.Equipes.Single(x => x.SoireeId == soireeID && x.teamNumber == curTransport.teamNumber).Id;
+                onrEntities.Transports.Add(curTransport);
+                transports.Add(curTransport);
+
+            }
+
+            onrEntities.SaveChanges();
+
+
+
+
+
+        }
+
+    }
+}
---
+++ OnrVideoWebJob/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace OnrVideoWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ONRVideo.ControlerJsonToModel ctlOnrVideo = new ONRVideo.ControlerJsonToModel();
+            string soireJSON = GetJSon("http://onrvideo.azurewebsites.net/soirees/soiree2281.txt");  //("https://eblais_test:1234qwer@onr-pilote.com/app/api/eveningStatistics?eveningId=2281");
+
+            ctlOnrVideo.RetrieveModelFromJSon(soireJSON);
+            
+        }
+
+
+        static private string GetJSon(string _url)
+        {
+            string retValue = string.Empty;
+
+            var request = WebRequest.Create(_url);
+            request.ContentType = "application/json; charset=utf-8";
+
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                retValue = sr.ReadToEnd();
+            }
+            ONRVideo.ControlerJsonToModel ctlOnrVideoJson = new ONRVideo.ControlerJsonToModel();
+
+            ctlOnrVideoJson.RetrieveModelFromJSon(retValue);
+
+            return retValue;
+
+        }
+    }
+}
